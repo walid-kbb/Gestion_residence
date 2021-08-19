762a79042769e1262b8fc39c8fd3a8029a2f156a
Walid KEBBAB <wkebbab@iliad.fr>
2021-08-20 00:50:48 +0200

    create service package

--- src/main/java/com/kebdev/residence_locations/repo/AdminRepo.java
+++ src/main/java/com/kebdev/residence_locations/repo/AdminRepo.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kebdev.residence_locations.model.Admin;
 
 public interface AdminRepo extends JpaRepository<Admin,Long>{
+
+	void deleteAdminById(Long id);
+
+	Admin findAdminById(Long id);
 	
 
 }
--- src/main/java/com/kebdev/residence_locations/service/AdminService.java
+++ src/main/java/com/kebdev/residence_locations/service/AdminService.java
@@ -1,5 +1,40 @@
 package com.kebdev.residence_locations.service;
 
-public class AdminService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.kebdev.residence_locations.model.Admin;
+import com.kebdev.residence_locations.repo.AdminRepo;
+
+@Service
+public class AdminService {
+	
+	private final AdminRepo adminRepo;
+	
+	public AdminService(AdminRepo adminRepo) {
+		this.adminRepo = adminRepo;
+	}
+
+	public Admin addAdmin(Admin admin) {
+		return adminRepo.save(admin);
+	}
+	
+	public List<Admin> getAdmins (){
+		return adminRepo.findAll();
+	}
+	
+	public Admin updateAdmin(Admin admin) {
+		return adminRepo.save(admin);
+	}
+	
+	public void deleteAdmin (Long id) {
+		adminRepo.deleteAdminById(id);
+		
+	}
+	
+	public Admin findAdmin (Long id) {
+		return adminRepo.findAdminById(id);
+	}
+	
 }
